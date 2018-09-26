96ff93d8419241948f6a19c009495962ea6412c5
rocksinghajay <rocksinghajay@gmail.com>
2018-09-27 00:37:27 +0530

    added tictactoe box and some functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import './App.css'
 
 class App extends Component {
+
+    state = {
+
+        board: Array(9).fill(null),
+        player: "X"
+
+    }
+
+    handleOnClick = index => {
+
+        const { board, player } = this.state
+
+        let newBoard = board
+        let newPlayer = player === "X" ? "O" : "X"
+
+        if(newBoard[index] === null) {
+
+            newBoard[index] = player
+
+            this.setState({
+                board:newBoard,
+                player:newPlayer
+
+            })
+        }
+    }
+
   render() {
+
+      const { board } = this.state
+      const boxes = board.map((box,index) => {
+          return (
+              <div key={index} className="box" onClick={ () => this.handleOnClick(index) }>{ box }</div>
+            )})
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <h1>TicTacToe Game</h1>
+
+        <div className="board">
+          { boxes }
+        </div>
+
       </div>
     );
   }
